2444498dc44c55a263b3bbd6e0e31f35e3b58fa8
KxllSwxtch <dimashin9@gmail.com>
2025-02-28 09:19:25 +0900

    updates

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,10 @@
 const Loader = () => {
 	return (
-		<div className='flex justify-center items-center h-full'>
-			<div className='w-12 h-12 border-4 border-t-arkGoldDark border-gray-300 rounded-full animate-spin'></div>
+		<div className='fixed inset-0 flex justify-center items-center bg-black bg-opacity-50 z-50'>
+			<div className='relative w-16 h-16'>
+				<div className='absolute inset-0 border-4 border-t-arkGoldDark border-transparent rounded-full animate-spin glow'></div>
+				<div className='absolute inset-0 border-4 border-b-arkGoldDark border-transparent rounded-full animate-spin-reverse glow'></div>
+			</div>
 		</div>
 	)
 }
